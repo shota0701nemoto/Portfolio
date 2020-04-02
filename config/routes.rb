0d2629436b9a1bbf08c563dd5a1e407a09005057
nemotoshogo <shota0701nemoto@gmail.com>
2020-04-02 13:51:08 +0900

    logoutが動かない

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
-  
-  
+
+
+
   mount Ckeditor::Engine => '/ckeditor'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
+
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -13,18 +13,18 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  
+
   resources :users
-  
+
   resources :account_activations, only: [:edit]
-  
+
   resources :posts,         only:  [:new, :show, :create, :destroy]
-  
+
   resources :comments,               only: [:create, :destroy]
-  
+
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  
+
   resources :gyms
-  
+
   resources :blogs
 end
