19eabcc5576cb42142d4afee24aa8f13f67fc8e1
nemotoshogo <shota0701nemoto@gmail.com>
2020-04-29 15:49:11 +0900

    likeモデルのテスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_094055) do
+ActiveRecord::Schema.define(version: 2020_04_29_035449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2020_04_12_094055) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "picture"
     t.index ["user_id"], name: "index_gyms_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "gym_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "pictures", force: :cascade do |t|
