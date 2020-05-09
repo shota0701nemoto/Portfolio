9faee39994926f77f4cdce27521375f8013b6627
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-09 22:16:53 +0900

    デプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,7 @@
 server '13.115.179.212', user: 'shogo', roles: %w{app db web}
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/Portfolio_aws_rsa'
+set :ssh_options,{
+keys: '~/.ssh/Portfolio_aws_rsa'
+port: 5432
+}
