f5404f6cfa5e7e14f75d80f8c2d04665ec2b8cf6
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-18 22:59:58 +0900

    blog.contentの文字数を30000に変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: { maximum: 38 }
-    validates :body, presence: true, length: { maximum: 9999 }
+    validates :body, presence: true, length: { maximum: 30000 }
 end
