35cca4757aeab45d39d324d718d210f944a4a5e7
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-18 22:10:50 +0900

    デザイン、機能の微調整

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
     belongs_to :user
+    validates :title, presence: true, length: { maximum: 38 }
+    validates :body, presence: true, length: { maximum: 9999 }
 end
