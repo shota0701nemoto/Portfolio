8122a3bb3951e91887779139dc3c607eed68d3f0
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-20 19:34:31 +0900

    いいねのデザイン修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,7 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
-    email: "example@railstutorial.org",
-    password:              "foobar",
-    password_confirmation: "foobar",
+User.create!(name:  "test",
+    email: "test@gmail.com",
+    password:              "test",
+    password_confirmation: "test",
     admin: true)
 
-# 追加のユーザーをまとめて生成する
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-      email: email,
-      password:              password,
-      password_confirmation: password)
-end
