ef17b5ded893844d0caee8d33f8e8e4e0443a9db
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-21 21:55:08 +0900

    テストを回す

--- spec/features/blogs_spec.rb
+++ spec/features/blogs_spec.rb
@@ -31,27 +31,28 @@ RSpec.feature "Blogs", type: :feature do
 
   it "投稿詳細ページからジム詳細ページに飛ぶことができる" do
     @related_blogs = create(:blog)
+    @related_gyms = create(:gym)
     @user = create(:user,email: "test2@example.com",)
     visit root_path
     click_link "ログイン"
     fill_in "session[email]", with: @user.email
     fill_in "session[password]", with: @user.password
     click_button "ログインボタン"
-    click_link "口コミ"
-    click_link "投稿する"
-    fill_in 'gym[name]', with: "Test Task"
-    fill_in 'gym[content]', with: "This is Test"
-    attach_file "gym_picture", "app/assets/images/test.png"
-    click_button '投稿する'
+    #click_link "口コミ"
+    #click_link "投稿する"
+    #fill_in 'gym[name]', with: "Test Task"
+    #fill_in 'gym[content]', with: "This is Test"
+    #attach_file "gym_picture", "app/assets/images/test.png"
+    #click_button '投稿する'
     click_link "コラム"
     click_on @related_blogs.title
-    expect(page).to have_content "Test Task"
+    expect(page).to have_content @related_gyms.name
     click_link "Test Task"
-    expect(page).to have_content "Test Task"
+    expect(page).to have_content "This is Test"
   end
 
   it "投稿詳細ページから他の投稿詳細ページには飛ばない" do
-    @blog = create(:blog)
+    @related_blogs = create(:blog)
     @user = create(:user,email: "test2@example.com",)
     visit root_path
     click_link "ログイン"
