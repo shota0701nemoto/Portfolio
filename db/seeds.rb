dec806aa1967bcc106f829907d137a0ad0720262
shota0701nemoto <shota0701nemoto@gmail.com>
2020-05-25 01:19:50 +0000

    seeds.rbとdatabase.ymlを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,7 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
-    email: "example@railstutorial.org",
-    password:              "foobar",
-    password_confirmation: "foobar",
+User.create!(name:  "AdminUser",
+             email: "Admin@Admin.com",
+    password:              "Admin",
+    password_confirmation: "Admin",
     admin: true)
 
-# 追加のユーザーをまとめて生成する
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-      email: email,
-      password:              password,
-      password_confirmation: password)
-end
