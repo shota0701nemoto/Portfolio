e55fca9872dbbc010bc82395e52e8ab9428a7cb6
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-31 12:38:37 +0900

    gymの他のuserがコメントしたのを削除できるか確認

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -95,12 +95,7 @@ RSpec.feature "Comments", type: :feature do
     fill_in "session[email]", with: @user.email
     fill_in "session[password]", with: @user.password
     click_button "ログインボタン"
-    #@userのgymを作成する
-    #@gym = Gym.create(
-    #name: "ゴールドジム",
-    #content: "良い",
-    #picture: "app/assets/images/test.png"
-  #)
+
     @gym = create(:gym,user:@other_user,)
     # タスク作成ページへ遷移
     click_link "口コミ"
@@ -111,6 +106,37 @@ RSpec.feature "Comments", type: :feature do
     end
 
     it"other_userがuserの作成したgymにコメントしたのを消す"do
+    @user = User.create(
+    name: "test",
+    email: "test@example.com",
+    password: "test"
+   )
+   @other_user = User.create(
+    name: "other_user",
+    email: "other_user@example.com",
+    password: "other_user"
+   )
+
+  # トップページへアクセス
+  visit root_path
+  # サインインページへ遷移
+  click_link "ログイン"
+  # メアドとパスワードを入力してログイン
+  fill_in "session[email]", with: @user.email
+  fill_in "session[password]", with: @user.password
+  click_button "ログインボタン"
+
+  @gym = create(:gym,user:@other_user,)
+  # タスク作成ページへ遷移
+  click_link "口コミ"
+  click_link "test"
+  fill_in 'comment[content]', with: "userのコメント"
+  click_button "コメントする"
+  expect(page).to have_content 'コメントが投稿されました'
+  click_button "コメントを表示"
+  click_button "削除"
+  page.driver.browser.switch_to.alert.accept
+  expect(page).to have_content 'userのコメント'
     end
 
     it"other_userがuserのコメントを消せない"do
