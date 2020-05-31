411b98a5097a61a712f71ba39e5eb2dbd0051539
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-31 12:59:25 +0900

    gymの他のuserがコメントしたのを削除できるか確認

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -135,9 +135,11 @@ RSpec.feature "Comments", type: :feature do
   expect(page).to have_content 'コメントが投稿されました'
   click_button "コメントを表示"
   click_link "削除する"
+
   page.accept_confirm do
-  click_link "OK"
+   link.click
   end
+  
   expect(page).to have_content 'userのコメント'
     end
 
