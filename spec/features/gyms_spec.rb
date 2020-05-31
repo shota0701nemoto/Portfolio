9335cb4e608f45b06ba1c42bd84c0488fb2c62e7
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-31 13:57:48 +0900

    人のコメントを削除できない

--- spec/features/gyms_spec.rb
+++ spec/features/gyms_spec.rb
@@ -32,6 +32,35 @@ RSpec.feature "Gyms", type: :feature do
   end
 
   it"userがgymを削除する"do
+  @user = User.create(
+    name: "PortfolioTaro",
+    email: "test@example.com",
+    password: "test"
+  )
+  @gym = create(:gym,user:@user,)
+  # トップページへアクセス
+  visit root_path
+  # サインインページへ遷移
+  click_link "ログイン"
+  # メアドとパスワードを入力してログイン
+  fill_in "session[email]", with: @user.email
+  fill_in "session[password]", with: @user.password
+  click_button "ログインボタン"
+  #@userがgymを投稿する
+  # タスク作成ページへ遷移
+  click_link "口コミ"
+  click_link "投稿する"
+  # 名前、口コミ、写真を投稿する
+  #fill_in 'gym[name]', with: "Test Task"
+  #fill_in 'gym[content]', with: "This is Test"
+  #attach_file "gym_picture", "app/assets/images/test.png"
+  #click_button '投稿する'
+  # 作成成功のメッセージが表示されること
+  #expect(page).to have_content '投稿ありがとうございます!'
+  click_link "口コミ"
+  click_link @gym.name
+  click_button "削除"
+  
   end
 
   it"other_userがuserのgymを削除できない"do
