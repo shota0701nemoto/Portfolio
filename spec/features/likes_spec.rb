4cee1566bcbb91efe39269f72f05072126e70d27
nemotoshogo <shota0701nemoto@gmail.com>
2020-05-31 14:31:14 +0900

    other_userがuserの投稿のいいねを取り消せない

--- spec/features/likes_spec.rb
+++ spec/features/likes_spec.rb
@@ -74,41 +74,85 @@ RSpec.feature "Likes", type: :feature do
     end
 
     it"other_userがuserの投稿にいいねする"do
+    @user = User.create(
+      name: "PortfolioTaro",
+      email: "test@example.com",
+      password: "test"
+    )
+    @other_user = create(:user,email: "test1@example.com",)
+
+    visit root_path
+
+    click_link "ログイン"
+
+    fill_in "session[email]", with: @user.email
+    fill_in "session[password]", with: @user.password
+    click_button "ログインボタン"
+
+    @gym = create(:gym,user:@other_user,)
+
+    click_link "口コミ"
+    click_on @gym.name
+    click_link 'いいね'
+    expect{
+      (page).to change(@gym.likes, :count).by(1)
+    }
     end
 
-    it"other_userがuserの投稿のいいねを取り消せない"do
-    end
+  it"other_userがuserの投稿のいいねを取り消せない"do
+  @user = User.create(
+    name: "PortfolioTaro",
+    email: "test@example.com",
+    password: "test"
+  )
+  @other_user = create(:user,email: "test1@example.com",)
 
-    it "userがother_userの投稿にいいねをした後、取り消す" do
-      @user = User.create(
-        name: "PortfolioTaro",
-        email: "test@example.com",
-        password: "test"
-      )
-      @other_user = create(:user,email: "test1@example.com",)
+  visit root_path
 
-        visit root_path
+  click_link "ログイン"
 
-        click_link "ログイン"
+  fill_in "session[email]", with: @user.email
+  fill_in "session[password]", with: @user.password
+  click_button "ログインボタン"
 
-        fill_in "session[email]", with: @user.email
-        fill_in "session[password]", with: @user.password
-        click_button "ログインボタン"
+  @gym = create(:gym,user:@other_user,)
 
-        @gym = create(:gym,user:@other_user,)
+  click_link "口コミ"
+  click_on @gym.name
+  expect(page).not_to have_content "取り消す"
 
-          click_link "口コミ"
-          click_on @gym.name
-          click_link 'いいね'
-          expect{
-            (page).to change(@gym.likes, :count).by(1)
-          }
-
-          expect(page).to have_content "取り消す"
-          click_link "取り消す"
-          expect{
-            (page).to change(@gym.likes, :count).by(0)
-          }
-end
-        end
   end
+
+it "userがother_userの投稿にいいねをした後、取り消す" do
+  @user = User.create(
+    name: "PortfolioTaro",
+    email: "test@example.com",
+    password: "test"
+  )
+  @other_user = create(:user,email: "test1@example.com",)
+
+  visit root_path
+
+  click_link "ログイン"
+
+  fill_in "session[email]", with: @user.email
+  fill_in "session[password]", with: @user.password
+  click_button "ログインボタン"
+
+  @gym = create(:gym,user:@other_user,)
+
+  click_link "口コミ"
+  click_on @gym.name
+  click_link 'いいね'
+  expect{
+    (page).to change(@gym.likes, :count).by(1)
+  }
+
+  expect(page).to have_content "取り消す"
+  click_link "取り消す"
+  expect{
+    (page).to change(@gym.likes, :count).by(0)
+  }
+end
+end
+end
