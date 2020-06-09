22d4983a4779a4c689cd7334cd833eaca101c591
nemotoshogo <shota0701nemoto@gmail.com>
2020-06-09 15:57:38 +0900

    rspec、gymのフォローボタンを削除

--- spec/features/relationships_spec.rb
+++ spec/features/relationships_spec.rb
@@ -3,66 +3,6 @@ require 'rails_helper'
 RSpec.feature "Relationships", type: :feature do
 
   describe "Relationships" do
-
-  it 'userがother_userをフォローする' do
-    # ユーザーを作成
-    @user = User.create(
-      name: "PortfolioTaro",
-      email: "test@example.com",
-      password: "test"
-    )
-    @other_user = create(:user,email: "test1@example.com",)
-    # トップページへアクセス
-    visit root_path
-    # サインインページへ遷移
-    click_link "ログイン"
-    # メアドとパスワードを入力してログイン
-    fill_in "session[email]", with: @user.email
-    fill_in "session[password]", with: @user.password
-    click_button "ログインボタン"
-
-    @gym = create(:gym,user:@other_user,)
-    # タスク作成ページへ遷移
-    click_link "口コミ"
-    click_link @gym.name
-
-    click_button "フォロー"
-
-    expect(page).to have_content 'ユーザーをフォローしました'
+  
   end
-
-  it 'userがother_userのフォローを外す' do
-    # ユーザーを作成
-    @user = User.create(
-      name: "PortfolioTaro",
-      email: "test@example.com",
-      password: "test"
-    )
-    @other_user = create(:user,email: "test1@example.com",)
-    # トップページへアクセス
-    visit root_path
-    # サインインページへ遷移
-    click_link "ログイン"
-    # メアドとパスワードを入力してログイン
-    fill_in "session[email]", with: @user.email
-    fill_in "session[password]", with: @user.password
-    click_button "ログインボタン"
-
-    @gym = create(:gym,user:@other_user,)
-    # タスク作成ページへ遷移
-    click_link "口コミ"
-    click_link @gym.name
-
-    click_button "フォロー"
-
-    expect(page).to have_content 'ユーザーをフォローしました'
-
-    click_link "口コミ"
-    click_link @gym.name
-
-    click_button "外す"
-
-    expect(page).to have_content 'ユーザーのフォローを解除しました'
-  end
-end
 end
