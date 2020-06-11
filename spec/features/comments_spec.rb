63fedd15756eebfdb36a8427c37530272831e63a
nemotoshogo <shota0701nemoto@gmail.com>
2020-06-11 11:03:09 +0900

    テストを回す

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Comments", type: :feature do
+RSpec.feature "Comments", js: true, type: :feature do
 
   describe "Comments" do
     it "userが自身で作成したgymにコメントをする" do
